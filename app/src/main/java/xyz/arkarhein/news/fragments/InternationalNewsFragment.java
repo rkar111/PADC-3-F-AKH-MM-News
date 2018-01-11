df490e5fe81e31c12d0b62fa2827df8a45e3ba24
Arkar Hein <rkarheinhein1@gmail.com>
2018-01-12 03:49:38 +0630

    SportsTabLayoutPOC added InternationalNewsTabLayoutPOC added

--- app/src/main/java/xyz/arkarhein/news/fragments/InternationalNewsFragment.java
+++ app/src/main/java/xyz/arkarhein/news/fragments/InternationalNewsFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import xyz.arkarhein.news.R;
+import xyz.arkarhein.news.adapters.InternationalNewsAdapter;
 
 /**
  * Created by Arkar Hein on 1/7/2018.
@@ -17,11 +21,22 @@ import xyz.arkarhein.news.R;
 
 public class InternationalNewsFragment extends Fragment {
 
+    @BindView(R.id.rv_international_news)
+    RecyclerView rvInternationalNews;
+
+    InternationalNewsAdapter mInternationalNewsAdapter = new InternationalNewsAdapter();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_international_news, container, false);
         ButterKnife.bind(this, view);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),
+                LinearLayoutManager.VERTICAL, false);
+        rvInternationalNews.setLayoutManager(linearLayoutManager);
+        rvInternationalNews.setAdapter(mInternationalNewsAdapter);
+
         return view;
     }
 }
--- app/src/main/java/xyz/arkarhein/news/fragments/SportsNewsFragment.java
+++ app/src/main/java/xyz/arkarhein/news/fragments/SportsNewsFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import xyz.arkarhein.news.R;
+import xyz.arkarhein.news.adapters.SportsNewsAdapter;
 
 /**
  * Created by Arkar Hein on 1/7/2018.
@@ -17,11 +21,22 @@ import xyz.arkarhein.news.R;
 
 public class SportsNewsFragment extends Fragment {
 
+    @BindView(R.id.rv_sports_news)
+    RecyclerView rvSportsNews;
+
+    SportsNewsAdapter mSportsNewsAdapter = new SportsNewsAdapter();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sports_news, container, false);
         ButterKnife.bind(this, view);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),
+                LinearLayoutManager.VERTICAL, false);
+        rvSportsNews.setLayoutManager(linearLayoutManager);
+        rvSportsNews.setAdapter(mSportsNewsAdapter);
+
         return view;
     }
 }
